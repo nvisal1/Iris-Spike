131da458f94febd38fd63f910fbcc821dabdc69f
Nick Visalli <nick@Nicks-MBP.home>
2019-01-13 11:58:18 -0500

    Updates

---
+++ precache-manifest.a1ef33f61eecc1e29ad70bef12fb2a3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "82341b758e83d1df32df",
+    "url": "/Iris-Spike/static/js/runtime~main.82341b75.js"
+  },
+  {
+    "revision": "72ae309bccd66facebd5",
+    "url": "/Iris-Spike/static/js/main.72ae309b.chunk.js"
+  },
+  {
+    "revision": "6200b412cbe4b022bd92",
+    "url": "/Iris-Spike/static/js/1.6200b412.chunk.js"
+  },
+  {
+    "revision": "72ae309bccd66facebd5",
+    "url": "/Iris-Spike/static/css/main.25c91827.chunk.css"
+  },
+  {
+    "revision": "be9ecd640b1caaa585ee176d3194bfc8",
+    "url": "/Iris-Spike/index.html"
+  }
+];
